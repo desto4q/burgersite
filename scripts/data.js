b0af5a3852ec98e6f221a88e0b4f17a4b90310c0
Destiny <desto4q@gmail.com>
2024-01-17 15:42:06 +0100

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,19 @@
+let links = [
+    {
+        to: "#",
+        name: "Home"
+    },
+    {
+        to: "#",
+        name: "Menu"
+    },
+    {
+        to: "#",
+        name: "About us"
+    },
+    {
+        to: "#",
+        name: "Contact"
+    },
+    
+]
---
+++ scripts/index.js
@@ -0,0 +1,75 @@
+let nav_bar = document.querySelector(".nav_links")
+let side_nav = document.querySelector(".side_nav")
+let nav_btn = document.querySelector(".nav_btn")
+let side_nav_content = document.querySelector(".side_nav_links")
+let a_tags = document.querySelectorAll("a")
+
+
+a_tags.forEach(a_tag => {
+    a_tag.addEventListener("click", (e) => {
+        e.preventDefault()
+    })
+
+})
+let div_maker = (type, content) => {
+    let div = document.createElement(`${type}`)
+    div.innerHTML = content
+    return div
+}
+
+let appender = (parent) => {
+    for (let i in links) {
+        let a = div_maker("a", links[i].name)
+        a.setAttribute("href", "#")
+        parent.append(a)
+    }
+}
+
+appender(nav_bar)
+appender(side_nav_content)
+
+
+
+
+let side_nav_activator = () => {
+    let shown = false
+    return {
+        switcher: () => {
+            if (shown == false) {
+                side_nav.classList.add("open")
+                shown = true
+                nav_btn.classList.add("checked")
+            }
+            else {
+                side_nav.classList.remove("open")
+                shown = false
+                nav_btn.classList.remove("checked")
+            }
+        },
+        nav_close: () => {
+            if (shown != false) {
+                side_nav.classList.remove("open")
+                shown = false
+                nav_btn.classList.remove("checked")
+            }
+        }
+    }
+
+}
+
+let nav_activate = side_nav_activator()
+
+
+nav_btn.onmousedown = () => {
+    nav_activate.switcher()
+}
+
+
+console.log(nav_bar)
+
+window.onresize = () => {
+
+    if (window.innerWidth > 600) {
+        nav_activate.nav_close()
+    }
+}
